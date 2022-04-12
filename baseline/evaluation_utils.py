8bf197cba55ca455713f7b69693b7854a6925609
Chen-Yiyang <iycspfa@gmail.com>
2022-04-12 22:07:52 +0800

    Implement helper function for evaluation metrics

--- baseline/evaluation_utils.py
+++ baseline/evaluation_utils.py
@@ -1,12 +1,15 @@
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score, classification_report
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sn
 
 classes = ['terrier', 'bulldog', 'schnauzer', 'sheepdog', 'retriever',
            'spaniel', 'sheepdog', 'setter', 'hound', 'poodle']
 
-def show_confusion_matrix(scores, y_true):
-    y_pred = scores.argmax(dim=1).numpy()
+def show_confusion_matrix(y_pred, y_true):
+    y_pred = np.asarray(y_pred)
+    y_true = np.asarray(y_true)
 
     # Build confusion matrix
     cf_matrix = confusion_matrix(y_true, y_pred)
@@ -15,3 +18,13 @@ def show_confusion_matrix(scores, y_true):
 
     plt.figure(figsize=(12, 7))
     sn.heatmap(df_cm, annot=True)
+
+def show_eval_metrics(y_pred, y_true, average="macro"):
+    y_pred = np.asarray(y_pred)
+    y_true = np.asarray(y_true)
+
+    print("F1 Score :", f1_score(y_true, y_pred, average="macro"))
+    print("Precision:", precision_score(y_true, y_pred, average="macro"))
+    print("Recall   :", recall_score(y_true, y_pred, average="macro"))
+
+
